aa4e9b573cb37d36a8914f01cdbd27d8388a0b62
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-28 12:54:35 +0700

    feat: create api to get post request with amount

--- app/api/vat/route.js
+++ app/api/vat/route.js
@@ -7,3 +7,14 @@ export async function GET(request) {
   }
   return Response.json({ rate: rate, amount: amount, vat: vat });
 }
+
+export async function POST(request) {
+  const data = await request.json();
+  const amount = data["amount"]
+  const rate = process.env.VAT_RATE;
+  let vat = null;
+  if (amount !== null) {
+    vat = (amount * rate).toFixed(2);
+  }
+  return Response.json({ rate: rate, amount: amount, vat: vat });
+}
